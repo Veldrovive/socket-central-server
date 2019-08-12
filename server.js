48d9c5fe4cd2604d2c96d4827b19eb72ae4494e7
Aidan Dempster <aidan.dempster@gmail.com>
2019-08-11 22:15:33 -0400

    Initial Commit This is a fully working version. However, it does not have persistant data and is very bare bones.

---
+++ server.js
@@ -0,0 +1,92 @@
+const config = require("./config");
+const io = require('socket.io')(81);
+const shortid = require("shortid");
+
+console.log("Server starting");
+
+const registered_clients = {};
+
+io.on("connection", (socket) => {
+	let name;
+	console.log("Client connecting");
+
+	socket.on("register", (data={}, ack) => {
+		({name} = data);
+		const exists = registered_clients.hasOwnProperty(name);
+		if(!exists){
+			registered_clients[name] = {
+				socket: socket,
+				id: shortid.generate(),
+				connected: true
+			}
+			socket.emit("registered");
+			console.log("Resistered new client",name);
+		}else if(!registered_clients[name].connected){
+			registered_clients[name].socket = socket;
+			registered_clients[name].connected = true;
+			socket.emit("registered");
+			ack(true);
+			console.log("Registered old client",name);
+		}else{
+			socket.emit("register_failed", {error: 450, message: "Client already connected"});
+			socket.disconnect();
+			console.log("Failed to register client",name,"since it was already connected");
+		}
+	})
+
+	socket.on("get_id", (data={}, ack) => {
+		let {appName} = data;
+		if(!appName) appName = name;
+		const client = registered_clients[appName];
+		if(client){
+			socket.emit("get_id_successful", client.id);
+			ack({success: true, id: client.id});
+		}else{
+			socket.emit("get_id_failed", {error: 440, message: "Client not registered"});
+			ack({success: false, error: 440, message: "Client not registered"});
+		}
+	})
+
+	socket.on("get_clients", (ack) => {
+		const clients = [];
+		Object.keys(registered_clients).forEach(name => {
+			clients.push([name, registered_clients[name].id, registered_clients[name].connected]);
+		})
+		socket.emit("get_clients_successful", clients);
+		ack(clients);
+	})
+
+	socket.on("command", (data={}, ack) => {
+		console.log("Relaying",data);
+		const {mirror=false, target, command, meta} = data;
+		if(target){
+			const client = registered_clients[target];
+			if(!client){
+				ack({success: false, error: 440, message: "Client not registered"});
+				return socket.emit("command_failed", {success: false, error: 440, message: "Client not registered"});
+			}else{
+				if(!client.connected) {
+					ack({success: false, error: 441, message: "Client not connected"});
+					return socket.emit("command_failed", {success: false, error: 441, message: "Client not connected"});
+				}
+				client.socket.emit("command", {command: command, meta: meta});
+				ack({success: true});
+				return socket.emit("command_successful");
+			}
+		}else{
+			if(mirror){
+				io.emit("command", {command: command, meta: meta});
+			}else{
+				socket.broadcast.emit("command", {command: command, meta: meta});
+			}
+			ack({success: true});
+			return socket.emit("command_successful");
+		}
+	})
+
+	socket.on("disconnect", () => {
+		registered_clients[name].connected = false;
+		io.emit("client_disconnect", {name: name, id: registered_clients[name].id});
+		console.log("Client disconnected:",name);
+	})
+})
---
+++ test_client.js
@@ -0,0 +1,57 @@
+var socket = require('socket.io-client')('http://localhost:81');
+console.log("Attempting to connect");
+
+socket.on("command", (data) => {
+	const {command, meta} = data;
+	console.log("Recieved command",command, meta);
+})
+
+function getId(){
+	return new Promise((resolve, reject) => {
+		if(socket.disconnected) return reject("No Connection");
+		socket.emit("get_id", {}, ack);
+		function ack(data){
+			const {success, id, error, message} = data;
+			if(success){
+				return resolve(id);
+			}else{
+				return reject({error: error, message, message});
+			}
+		}
+	})
+}
+
+function sendCommand(){
+	return new Promise((resolve, reject) => {
+		if(socket.disconnected) return reject("No Connection");
+		socket.emit("command", {command: "test_command", meta: [1, 2, 3], mirror: true}, ack);
+		function ack(data){
+			const {success, error, message} = data;
+			if(success){
+				return resolve();
+			}else{
+				return reject({error: error, message, message});
+			}
+		}
+	})
+}
+
+async function test(){
+	console.log("Testing id");
+	const id = await getId();
+	console.log(id);
+
+	console.log("Testing command");
+	await sendCommand();
+	console.log("Sent command");
+}
+
+socket.on("connect", () => {
+	console.log("Connected to server");
+	socket.emit("register", {name: "test"});
+	test();
+})
+
+socket.on("disconnect", () => {
+	console.log("Disconnected from server");
+})
